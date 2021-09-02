5a31f88324282e3bf725b32e6bae6f47c65eba18
Gargi Vatturkar <vgargiu@gmail.com>
2021-09-02 10:38:58 -0700

    10.40pm details changes

--- stack-overflow/src/app/details/details.component.ts
+++ stack-overflow/src/app/details/details.component.ts
@@ -19,9 +19,13 @@ export class DetailsComponent implements OnInit {
     this.postDetail['active_date'] = this.calculateDays(this.postDetail['last_activity_date']);
     this.postDetail['edited_date'] = this.calculateDays(this.postDetail['last_edit_date']);
 
-    if(this.postDetail.answers.length > 10)
-      this.answerList = this.postDetail.answers.slice(0, 10);
-    else this.answerList = this.postDetail.answers;
+    console.log(this.postDetail)
+    if(this.postDetail.answer_count > 0){
+      if(this.postDetail.answers.length > 10)
+        this.answerList = this.postDetail.answers.slice(0, 10);
+      else this.answerList = this.postDetail.answers;
+    }
+    
   }
 
   calculateDays(unixTime){
